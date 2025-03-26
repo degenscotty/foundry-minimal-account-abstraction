95dda465e3c467cb8073653c1e6a5abe2882e1bc
degenscotty <scottvermast@gmail.com>
2025-03-26 11:37:10 +0100

    edit: minimalaccount.sol

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -9,6 +9,11 @@ import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
 import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
+/**
+ * @title MinimalAccount
+ * @notice A minimal account contract that allows users to send transactions to the EntryPoint.
+ * @dev Inherits from IAccount and Ownable to manage user operations securely.
+ */
 contract MinimalAccount is IAccount, Ownable {
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
@@ -29,12 +34,59 @@ contract MinimalAccount is IAccount, Ownable {
         i_entryPoint = IEntryPoint(entryPoint);
     }
 
-    // A signature is valid, if it's the MinimalAccount owner
+    /**
+     * @notice Validates a user operation.
+     * @param userOp The packed user operation to validate.
+     * @param userOpHash The hash of the user operation.
+     * @param missingAccountFunds The amount of funds missing for the operation.
+     * @return validationData The validation result.
+     */
     function validateUserOp(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash,
         uint256 missingAccountFunds
     ) external returns (uint256 validationData) {
-        _validateSignature(userOp, userOpHash);
+        validationData = _validateSignature(userOp, userOpHash);
+        // _validateNonce()
+        _payPrefund(missingAccountFunds);
+    }
+
+    /**
+     * @notice Validates the signature of a user operation.
+     * @param userOp The packed user operation containing the signature.
+     * @param userOpHash The hash of the user operation.
+     * @return validationData The validation result.
+     */
+    function _validateSignature(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash
+    ) internal view returns (uint256 validationData) {
+        if (userOp.sender != address(this))
+            revert MinimalAccount__NotFromEntryPoint();
+        if (userOp.initCode.length > 0)
+            revert MinimalAccount__NotFromEntryPointOrOwner();
+
+        bytes32 messageHash = MessageHashUtils.toEthSignedMessageHash(
+            userOpHash
+        );
+        address signer = ECDSA.recover(messageHash, userOp.signature);
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    /**
+     * @notice Pays the prefund amount if missingAccountFunds is greater than zero.
+     * @param missingAccountFunds The amount of funds to pay.
+     */
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds > 0) {
+            (bool success, ) = payable(msg.sender).call{
+                value: missingAccountFunds,
+                gas: type(uint256).max
+            }("");
+            (success);
+        }
     }
 }
